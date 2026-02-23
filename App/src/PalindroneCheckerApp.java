326dd44734d2a7750acab9701c2986a2dc952adf
Virajjj646 <viraj.deglurkar@gmail.com>
2026-02-23 09:48:24 +0530

    UC5: Stack-Based Palindrome Checker

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
+import java.util.Stack;
+
 public class PalindroneCheckerApp {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        String input = "radar";
-        char[] chars = input.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
+        String input = "noon";
+        Stack<Character> stack = new Stack<>();
+        for(char c : input.toCharArray()){
+            stack.push(c);
+        }
         boolean isPalindrome = false;
-        while(start<end){
-            if(chars[start] == chars[end]){
+        for(char c : input.toCharArray()){
+            char poppedChar = stack.pop();
+            if(poppedChar!=c){
                 isPalindrome = true;
             }
-            start++;
-            end--;
         }
+
         System.out.println("Input: " + input);
         System.out.println("is Palindrome? " + isPalindrome);
     }
